9e0c620ad3cb187bfd57cdfde16e0c588bdb49ac
Richard Waller <Richard.Waller@ibm.com>
2019-12-08 14:21:29 +0000

    feat: complete day 1

--- src/2019/01/index.test.ts
+++ src/2019/01/index.test.ts
@@ -1,9 +1,42 @@
-import { sum } from '.';
+import { readFileSync } from 'fs-extra';
+import { resolve } from 'path';
 
-test('basic', () => {
-  expect(sum()).toBe(0)
+import {
+  calculateFuelRequiredForMass,
+  calculateFuelRequiredForMasses,
+  calculateFuelRequiredForMassAndItsFuel,
+  calculateFuelRequiredForMassesAndTheirFuel,
+} from '.';
+
+describe('part 1', () => {
+  test('calculateFuelRequiredForMass(mass)', () => {
+    expect(calculateFuelRequiredForMass(12)).toBe(2);
+    expect(calculateFuelRequiredForMass(14)).toBe(2);
+    expect(calculateFuelRequiredForMass(1969)).toBe(654);
+    expect(calculateFuelRequiredForMass(100756)).toBe(33583);
+  });
+  test('calculateFuelRequiredForMasses', () => {
+    const input = readFileSync(resolve(__dirname, 'part1TestInput.txt'), 'utf8');
+    expect(calculateFuelRequiredForMasses(input)).toBe(2 + 2 + 654 + 33583);
+  });
+  test('answer', () => {
+    const input = readFileSync(resolve(__dirname, 'realInput.txt'), 'utf8');
+    expect(calculateFuelRequiredForMasses(input)).toBe(3382284);
+  });
 });
 
-test('basic again', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('part 2', () => {
+  test('calculateFuelRequiredForMassAndItsFuel(mass)', () => {
+    expect(calculateFuelRequiredForMassAndItsFuel(14)).toBe(2);
+    expect(calculateFuelRequiredForMassAndItsFuel(1969)).toBe(966);
+    expect(calculateFuelRequiredForMassAndItsFuel(100756)).toBe(50346);
+  });
+  test('calculateFuelRequiredForMassesAndTheirFuel', () => {
+    const input = readFileSync(resolve(__dirname, 'part2TestInput.txt'), 'utf8');
+    expect(calculateFuelRequiredForMassesAndTheirFuel(input)).toBe(2 + 966 + 50346);
+  });
+  test('answer', () => {
+    const input = readFileSync(resolve(__dirname, 'realInput.txt'), 'utf8');
+    expect(calculateFuelRequiredForMassesAndTheirFuel(input)).toBe(5070541);
+  });
 });
--- src/2019/01/index.ts
+++ src/2019/01/index.ts
@@ -1,3 +1,32 @@
-export const sum
-  = (...a: number[]) =>
-    a.reduce((acc, val) => acc + val, 0)
+const calculateFuelRequiredForMass = (mass) => Math.floor(mass / 3) - 2;
+
+const sum = (array) => array.reduce((a, b) => a + b, 0);
+
+const calculateFuelRequiredForMasses = (inputString) => {
+  const moduleMasses = inputString.trim().split('\n');
+  const fuelRequired = moduleMasses.map(calculateFuelRequiredForMass);
+  return sum(fuelRequired);
+};
+
+const calculateFuelRequiredForMassAndItsFuel = (mass) => {
+  let totalFuelRequired = 0;
+  let fuelRequiredForMass = calculateFuelRequiredForMass(mass);
+  while (fuelRequiredForMass > 0) {
+    totalFuelRequired += fuelRequiredForMass;
+    fuelRequiredForMass = calculateFuelRequiredForMass(fuelRequiredForMass);
+  }
+  return totalFuelRequired;
+};
+
+const calculateFuelRequiredForMassesAndTheirFuel = (inputString) => {
+  const moduleMasses = inputString.trim().split('\n');
+  const fuelRequired = moduleMasses.map(calculateFuelRequiredForMassAndItsFuel);
+  return sum(fuelRequired);
+};
+
+export {
+  calculateFuelRequiredForMass,
+  calculateFuelRequiredForMasses,
+  calculateFuelRequiredForMassAndItsFuel,
+  calculateFuelRequiredForMassesAndTheirFuel,
+};
