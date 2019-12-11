6bd50b5a4da22132dff02ba99f847fb1ada944d7
Richard Waller <Richard.Waller@ibm.com>
2019-12-11 17:20:37 +0000

    chore: add template answer

---
+++ src/2019/Template/index.test.ts
@@ -0,0 +1,16 @@
+import {
+
+} from '.';
+
+describe('day X', () => {
+  describe('part 1', () => {
+    describe('', () => {
+      test('', () => {
+        expect().toEqual();
+        expect().toStrictEqual();
+      });
+    });
+  });
+  describe('part 2', () => {
+  });
+});
---
+++ src/2019/Template/index.ts
@@ -0,0 +1,7 @@
+const foo = () => {
+
+};
+
+export {
+
+};
