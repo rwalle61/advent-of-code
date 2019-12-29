e44e0c278f24987578a54485ba037400a4b7cf56
Richard Waller <Richard.Waller@ibm.com>
2019-12-29 15:54:26 +0000

    feat: complete day 6 parts 1 & 2

---
+++ src/2019/06/index.test.ts
@@ -0,0 +1,126 @@
+import { resolve } from 'path';
+
+import {
+  parseOrbitMap,
+  countOrbits,
+  countOrbitalTransfers,
+} from '.';
+
+const pathToDay6Dir = resolve('src', '2019', '06');
+
+describe('day 6', () => {
+  describe('input file parser', () => {
+    describe('parseOrbitMap(<filePath>)', () => {
+      test('part1ExampleInput', () => {
+        const filepath = resolve(pathToDay6Dir, 'part1ExampleInput.txt');
+        expect(parseOrbitMap(filepath)).toStrictEqual(
+          [
+            ['COM', 'B'],
+            ['B', 'C'],
+            ['C', 'D'],
+            ['D', 'E'],
+            ['E', 'F'],
+            ['B', 'G'],
+            ['G', 'H'],
+            ['D', 'I'],
+            ['E', 'J'],
+            ['J', 'K'],
+            ['K', 'L'],
+          ],
+        );
+      });
+    });
+  });
+  describe('part 1', () => {
+    describe('countOrbits(<orbitMap>)', () => {
+      test('simplest map: 1 orbit', () => {
+        const orbitMap = [['COM', 'B']];
+        expect(countOrbits(orbitMap)).toEqual(1);
+      });
+      test('indirect orbit', () => {
+        const orbitMap = [
+          ['COM', 'B'],
+          ['B', 'C'],
+        ];
+        expect(countOrbits(orbitMap)).toEqual(3);
+      });
+      test('fork', () => {
+        const orbitMap = [
+          ['COM', 'B'],
+          ['COM', 'C'],
+        ];
+        expect(countOrbits(orbitMap)).toEqual(2);
+      });
+      test('indirect orbit with fork', () => {
+        const orbitMap = [
+          ['COM', 'B'],
+          ['COM', 'C'],
+          ['C', 'D'],
+        ];
+        expect(countOrbits(orbitMap)).toEqual(4);
+      });
+      test('orbitMap where COM orbit is not listed first', () => {
+        const orbitMap = [
+          ['B', 'C'],
+          ['COM', 'B'],
+        ];
+        expect(countOrbits(orbitMap)).toEqual(3);
+      });
+      test('part1ExampleInput', () => {
+        const filepath = resolve(pathToDay6Dir, 'part1ExampleInput.txt');
+        const orbitMap = parseOrbitMap(filepath);
+        expect(countOrbits(orbitMap)).toEqual(42);
+      });
+      test('answer part 1', () => {
+        const filepath = resolve(pathToDay6Dir, 'puzzleInput.txt');
+        const orbitMap = parseOrbitMap(filepath);
+        expect(countOrbits(orbitMap)).toEqual(200001);
+      });
+    });
+  });
+  describe('part 2', () => {
+    describe('countOrbitalTransfers(<orbitMap>)', () => {
+      test('1 orbital transfer after you', () => {
+        const orbitMap = [
+          ['COM', 'YOU'],
+          ['YOU', 'SAN'],
+        ];
+        expect(countOrbitalTransfers(orbitMap)).toEqual(1);
+      });
+      test('2 orbital transfers after you', () => {
+        const orbitMap = [
+          ['COM', 'YOU'],
+          ['YOU', 'B'],
+          ['B', 'SAN'],
+        ];
+        expect(countOrbitalTransfers(orbitMap)).toEqual(2);
+      });
+      test('1 orbital transfer before you', () => {
+        const orbitMap = [
+          ['COM', 'SAN'],
+          ['SAN', 'YOU'],
+        ];
+        expect(countOrbitalTransfers(orbitMap)).toEqual(1);
+      });
+      test('orbital transfers to another fork', () => {
+        const orbitMap = [
+          ['COM', 'B'],
+          ['B', 'YOU'],
+          ['COM', 'C'],
+          ['C', 'SAN'],
+        ];
+        expect(countOrbitalTransfers(orbitMap)).toEqual(2);
+      });
+      test('part2ExampleInput', () => {
+        const filepath = resolve(pathToDay6Dir, 'part2ExampleInput.txt');
+        const orbitMap = parseOrbitMap(filepath);
+        expect(countOrbitalTransfers(orbitMap)).toEqual(4);
+      });
+      test('answer part 2', () => {
+        const filepath = resolve(pathToDay6Dir, 'puzzleInput.txt');
+        const orbitMap = parseOrbitMap(filepath);
+        expect(countOrbitalTransfers(orbitMap)).toEqual(379);
+      });
+    });
+  });
+});
---
+++ src/2019/06/index.ts
@@ -0,0 +1,54 @@
+import { readFileSync } from 'fs';
+
+const parseOrbitMap = (filepath) => {
+  const fileData = readFileSync(filepath, 'utf8').trim();
+  const mapOfStrings = fileData.split('\n');
+  const orbitMap = mapOfStrings.map((orbitString) => orbitString.split(')'));
+  return orbitMap;
+};
+
+const sumArray = (arr) => arr.reduce((a, b) => a + b, 0);
+
+const countOrbitsRecursive = (orbitMap, nextObj, distanceFromCentre) => {
+  const numOrbitsFromHere = distanceFromCentre;
+
+  const nextPairs = orbitMap.filter((pair) => pair[0] === nextObj);
+  const arrayOfOrbitsFromAfterHere = nextPairs.map((pair) =>
+    countOrbitsRecursive(orbitMap, pair[1], distanceFromCentre + 1));
+  const numOrbitsFromAfterHere = sumArray(arrayOfOrbitsFromAfterHere);
+
+  return numOrbitsFromHere + numOrbitsFromAfterHere;
+};
+
+const countOrbits = (orbitMap) => {
+  const firstNextObj = 'COM';
+  const numOrbits = countOrbitsRecursive(orbitMap, firstNextObj, 0);
+  return numOrbits;
+};
+
+const buildTreeBackwards = (orbitMap, currentOrbitingObj) => {
+  if (currentOrbitingObj === 'COM') {
+    return ['COM'];
+  }
+  const currentPair = orbitMap.find((pair) => pair[1] === currentOrbitingObj);
+  const nextOrbitingObj = currentPair[0];
+  const remainingTree = buildTreeBackwards(orbitMap, nextOrbitingObj);
+  return [currentOrbitingObj].concat(remainingTree);
+};
+
+const countOrbitalTransfers = (orbitMap) => {
+  const orbitTreeToYou = buildTreeBackwards(orbitMap, 'YOU');
+  const orbitTreeToSanta = buildTreeBackwards(orbitMap, 'SAN');
+  const nearestJunction = orbitTreeToYou.find((obj) =>
+    !['YOU', 'SAN'].includes(obj) && orbitTreeToSanta.includes(obj));
+  const transfersFromYouToNearestJunction = orbitTreeToYou.indexOf(nearestJunction) - 1;
+  const transfersFromNearestJunctionToSanta = orbitTreeToSanta.indexOf(nearestJunction) - 1;
+  const numTransfers = transfersFromYouToNearestJunction + transfersFromNearestJunctionToSanta;
+  return numTransfers;
+};
+
+export {
+  parseOrbitMap,
+  countOrbits,
+  countOrbitalTransfers,
+};
